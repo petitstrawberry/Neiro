2248d5dcad32395241e896b68676bca1ce2f0fbd
PetitStrawberry <petitstrawberry.dev@gmail.com>
2021-09-04 12:40:21 +0900

    clean: UserDataObject.swift

--- Shared/ObjectModels/UserDataObject.swift
+++ Shared/ObjectModels/UserDataObject.swift
@@ -21,24 +21,11 @@ class UserDataObject: ObservableObject {
 
     // savedServersの保存
     func storeSavedServers() {
-//        var urls: [String] = []
-//        savedServers.forEach { server in
-//            if let url = server.url?.absoluteString {
-//                urls.append(url)
-//            }
-//        }
         let archivedData = try! NSKeyedArchiver.archivedData(withRootObject: savedServers, requiringSecureCoding: false)
         UserDefaults.standard.set(archivedData, forKey: "savedServers")
     }
     // savedServersの読み込み
     func loadSavedServers() {
-//        if let urls = UserDefaults.standard.stringArray(forKey: "savedServers") {
-//            urls.forEach { url in
-//                let server = SavedServer()
-//                server.url = URL(string: url)
-//                savedServers.append(server)
-//            }
-//        }
         if let storedData = UserDefaults.standard.object(forKey: "savedServers") as? Data {
             if let unarchivedObject = try! NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(storedData) as? [UPPMediaServerDevice] {
                 savedServers = unarchivedObject
